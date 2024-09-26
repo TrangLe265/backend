193faa16d82980950c42a3dcd0364554b95b4531
TrangLe265 <144318730+TrangLe265@users.noreply.github.com>
2024-09-26 13:24:01 +0300

    Delete bookstoreREST directory

--- bookstoreREST/src/main/java/fi/haagahelia/bookstore/domain/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package fi.haagahelia.bookstore.domain;
-
-import org.springframework.data.repository.CrudRepository;
-public interface BookRepository extends CrudRepository<Book,Long>{
-//BookRepository an interface extending one of Spring Data's repository interfaces
-//It provides data access methods such as findAll(), saveAll
-//<Book,Long> : meaning this is a repository for the Book class and the ID type is long
-}
--- bookstoreREST/src/main/java/fi/haagahelia/bookstore/domain/CategoryRepository.java
+++
@@ -1,5 +0,0 @@
-package fi.haagahelia.bookstore.domain;
-import org.springframework.data.repository.CrudRepository;
-public interface CategoryRepository extends CrudRepository<Category,Long > {
-    //<Class name, Primary key of the class>
-}
